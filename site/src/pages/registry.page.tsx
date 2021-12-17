c166e56b9eb3dc1d0aa69b4f5c0637b11629ec9d
espretto <espretto@posteo.net>
2021-12-17 12:39:11 +0100

    test: if vercel lets me read files outside the deployment's root dir

--- site/src/pages/registry.page.tsx
+++ site/src/pages/registry.page.tsx
@@ -1,23 +1,21 @@
 import { GetStaticProps } from "next";
 import React from "react";
 
-/**
- * used to create an index of all available blocks, the catalog
- */
 export const getStaticProps: GetStaticProps = async () => {
   /* eslint-disable global-require -- dependencies are required at runtime to avoid bundling them w/ nextjs */
-  // const fs = require("fs");
+  const fs = require("fs");
   const glob = require("glob");
   /* eslint-enable global-require */
 
   return {
-    props: { paths: glob.sync(`${process.cwd()}/../registry/**/*.json`) },
+    props: {
+      paths: glob
+        .sync(`${process.cwd()}/../registry/**/*.json`)
+        .map((path: string) =>
+          JSON.parse(fs.readFileSync(path, { encoding: "utf8" })),
+        ),
+    },
   };
-  // .map((path: string) => ({
-  //   // @todo should be redundant to block's package.json#name
-  //   packagePath: path.split("/").slice(-3, -1).join("/"),
-  //   ...JSON.parse(fs.readFileSync(path, { encoding: "utf8" })),
-  // }));
 };
 
 const Registry: React.VFC<{ paths: string[] }> = ({ paths }) => {
